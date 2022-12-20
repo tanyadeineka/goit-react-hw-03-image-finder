b05541c85f5dd31b42dd4c1d4bd7aff3d7fbf4fc
Tetiana Deineka <108138671+tanyadeineka@users.noreply.github.com>
2022-12-21 00:15:08 +0200

    Update ImageGallery.jsx

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -2,15 +2,13 @@ import { ImageGalleryItem } from 'components/ImageGalleryItem/ImageGalleryItem';
 import propTypes from 'prop-types';
 import css from './ImageGallery.module.css';
 
-export const ImageGallery = ({ images, onImageClick }) => {
-  return (
+export const ImageGallery = ({ images, onImageClick }) => (
     <ul className={css.ImageGallery}>
       {images.map((image, index) => (
         <ImageGalleryItem onClick={onImageClick} image={image} key={index} />
       ))}
     </ul>
-  );
-};
+);
 
 ImageGalleryItem.propTypes = {
   images: propTypes.arrayOf(
