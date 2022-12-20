8558da6b9f957726dfa183067a23f26c326bfa74
Tetiana Deineka <tata4ka666@i.ua>
2022-12-20 23:45:39 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,100 @@
-import { Component } from "react";
-import React from "react";
-//import { fetchImages } from "./pixabay-api";
+import React, { Component } from "react";
 import { Searchbar } from "./Searchbar/Searchbar"
+import { fetchImages } from "./pixabay-api";
+import { ImageGallery } from "./ImageGallery/ImageGallery";
+import { Button } from "./Button/Button";
+import { Loader } from './Loader/Loader';
+import { Modal } from './Modal/Modal';
 
 export class App extends Component {
+  state = {
+    images: [],
+    isLoading: false,
+    currentSearch: '',
+    pageNumber: 1,
+    modalOpen: false,
+    modalImg: '',
+    modalAlt: '',
+  };
+
+  handleSubmit = async event => {
+    event.preventDefault();
+    this.setState({ isLoading: true });
+    const inputForSearch = event.target.elements.inputForSearch;
+    if (inputForSearch.value.trim() === '') {
+      return;
+    }
+    const response = await fetchImages(inputForSearch.value, 1);
+    this.setState({
+      image: response,
+      isLoading: false,
+      currentSearch: inputForSearch.value,
+      pageNumber: 1,
+    });
+  };
+
+  handleImageClick = event => {
+    this.setState({
+      modalOpen: true,
+      modalAlt: event.target.alt,
+      modalImg: event.target.name,
+    });
+  };
+
+  handleClickMore = async () => {
+    const response = await fetchImages(
+      this.state.currentSearch,
+      this.state.pageNumber + 1
+    );
+    this.setState({
+      images: [...this.state.images, ...response],
+      pageNr: this.state.pageNumber + 1,
+    });
+  };
+
+  handleModalClose = () => {
+    this.setState({
+      modalOpen: false,
+      modalImg: '',
+      modalAlt: '',
+    });
+  };
+
+  handleKeyDown = event => {
+    if (event.code === 'Escape') {
+      this.handleModalClose();
+    }
+  };
+
+  async componentDidMount() {
+    window.addEventListener('keydown', this.handleKeyDown);
+  }
+
   render() {
     return (
       <>
-        <Searchbar />
+        {this.state.isLoading ? (
+          <Loader />
+        ) : (
+          <React.Fragment>
+            <Searchbar onSubmit={this.handleSubmit} />
+            <ImageGallery
+              images={this.state.images}
+              onImageClick={this.handleImageClick}
+            />
+            {this.state.images.length > 0 ? (
+              <Button onClick={this.handleClickMore} />
+            ) : null}
+          </React.Fragment>
+        )}
+        {this.state.modalOpen ? (
+          <Modal
+            src={this.state.modalImg}
+            alt={this.state.modalAlt}
+            handleClose={this.handleModalClose}
+          />
+        ) : null}
       </>
-    )
+    );
   }
 }
