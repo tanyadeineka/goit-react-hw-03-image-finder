76868f143f11bdfcda43bb107d76a9c33a4c2451
Tetiana Deineka <tata4ka666@i.ua>
2022-12-20 23:05:40 +0200

    Create ImageGallery.jsx

---
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -0,0 +1,22 @@
+import { ImageGalleryItem } from 'components/ImageGalleryItem/ImageGalleryItem';
+import propTypes from 'prop-types';
+import css from './ImageGallery.module.css';
+
+export const ImageGallery = ({ images, onImageClick }) => {
+  return (
+    <ul className={css.ImageGallery}>
+      {images.map((image, index) => (
+        <ImageGalleryItem onClick={onImageClick} image={image} key={index} />
+      ))}
+    </ul>
+  );
+};
+
+ImageGalleryItem.propTypes = {
+  images: propTypes.arrayOf(
+    propTypes.shape({
+      id: propTypes.number.isRequired,
+    })
+  ),
+  onImageClick: propTypes.func.isRequired,
+};
