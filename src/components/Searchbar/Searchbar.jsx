011d30f58a095cf036197666a4d1b002db868dee
Tetiana Deineka <tata4ka666@i.ua>
2022-12-20 22:25:58 +0200

    Update Searchbar.jsx

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,20 +1,25 @@
-import css from "./Searchbar.module.css";
+import propTypes from 'prop-types';
+import css from './Searchbar.module.css';
 
-export const Searchbar = () => {
-    return (
-      <header className={css.searchbar}>
-        <form className={css.searchForm}>
-          <button type="submit" className={css.searchBtn}>
-            <span className={css.searchBtnLabel}>Search</span>
-          </button>
-          <input
-            className={css.searchInput}
-            type="text"
-            autocomplete="off"
-            autofocus
-            placeholder="Search images and photos"
-          />
-        </form>
-      </header>
-    );
-}
+export const Searchbar = ({ onSubmit }) => {
+  return (
+    <header className={css.searchbar}>
+      <form className={css.searchForm} onSubmit={onSubmit}>
+        <button type="submit" className={css.searchBtn}>
+          <span className={css.searchBtnLabel}>Search</span>
+        </button>
+        <input
+          className={css.searchInput}
+          type="text"
+          autocomplete="off"
+          autofocus
+          placeholder="Search images and photos"
+        />
+      </form>
+    </header>
+  );
+};
+
+Searchbar.propTypes = {
+  onSubmit: propTypes.func,
+};
