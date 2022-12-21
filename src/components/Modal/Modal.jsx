184ede66684eab600ede0e542068b9a337dabb2a
Tetiana Deineka <tata4ka666@i.ua>
2022-12-21 21:19:08 +0200

    Update Modal.jsx

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,13 +1,39 @@
+import React, { Component } from 'react';
 import css from './Modal.module.css';
 import propTypes from 'prop-types';
 
-export const Modal = ({ src, alt, handleClose }) => (
-  <div className={css.Overlay} onClick={handleClose}>
-    <div className={css.Modal}>
-      <img src={src} alt={alt} />
-    </div>
-  </div>
-);
+export class Modal extends Component {
+  componentDidMount() {
+    window.addEventListener('keydown', this.handleKeyDown);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('keydown', this.handleKeyDown);
+  }
+
+  handleKeyDown = e => {
+    if (e.code === 'Escape') {
+      this.props.onCloseModal();
+    }
+  };
+
+  handleClickOverlay = e => {
+    if (e.currentTarget === e.target) {
+      this.props.onCloseModal();
+    }
+  };
+
+  render() {
+    const { src, alt } = this.props;
+    return (
+      <div className={css.Overlay} onClick={this.handleClickOverlay}>
+        <div className={css.Modal}>
+          <img src={src} alt={alt} />
+        </div>
+      </div>
+    );
+  }
+} 
 
 Modal.propTypes = {
   src: propTypes.string.isRequired,
