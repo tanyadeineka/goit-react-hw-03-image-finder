c64276d570dc9ecb25b6259fbcf116346578b500
Tetiana Deineka <tata4ka666@i.ua>
2022-12-22 22:35:50 +0200

    Update Loader.jsx

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -2,15 +2,17 @@ import { Dna } from 'react-loader-spinner';
 
 import css from './Loader.module.css';
 
-export const Loader = () => (
-  <div className={css.Loader}>
-    <Dna
-      visible={true}
-      height="300"
-      width="300"
-      ariaLabel="dna-loading"
-      wrapperStyle={{}}
-      wrapperClass="dna-wrapper"
-    />
-  </div>
-);
+export const Loader = () => {
+  return (
+    <div className={css.Loader}>
+      <Dna
+        visible={true}
+        height="300"
+        width="300"
+        ariaLabel="dna-loading"
+        wrapperStyle={{}}
+        wrapperClass="dna-wrapper"
+      />
+    </div>
+  );
+}
