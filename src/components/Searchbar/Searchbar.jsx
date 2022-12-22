8a97b0b425b9308dcf0dd9e0515f479f9cbc93c4
Tetiana Deineka <tata4ka666@i.ua>
2022-12-22 19:06:10 +0200

    Update Searchbar.jsx

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,24 +1,51 @@
-import propTypes from 'prop-types';
 import css from './Searchbar.module.css';
+import { Component } from 'react';
 
-export const Searchbar = ({ onSubmit }) => (
-  <header className={css.Searchbar}>
-    <form className={css.SearchForm} onSubmit={onSubmit}>
-      <button type="submit" className={css.SearchFormButton}>
-        <span className={css.SearchFormButtonLabel}>Search</span>
-      </button>
-      <input
-        name="inputForSearch"
-        className={css.SearchFormInput}
-        type="text"
-        autoComplete="off"
-        autoFocus
-        placeholder="Search images and photos"
-      />
-    </form>
-  </header>
-);
+export class Searchbar extends Component {
+  state = {
+    query: '',
+    page: 1,
+    images: [],
+  };
 
-Searchbar.propTypes = {
-  onSubmit: propTypes.func,
-};
+  handleChange = event => {
+    this.setState({ query: event.currentTarget.value });
+  }
+
+  handleSubmit = event => {
+    event.preventDefault();
+
+    if (this.state.query.trim() === '') {
+      alert('Please, enter anything to search');
+      return;
+    }
+    this.props.onSubmit(this.state);
+    this.reset();
+  };
+
+  reset = () => {
+    this.setState({ query: '' });
+  };
+
+  render() {
+    return (
+      <header className={css.Searchbar}>
+        <form className={css.SearchForm} onSubmit={this.handleSubmit}>
+          <button type="submit" className={css.SearchFormButton}>
+            <span className={css.SearchFormButtonLabel}>Search</span>
+          </button>
+          <input
+            value={this.state.query}
+            onChange={this.handleChange}
+            name="inputForSearch"
+            className={css.SearchFormInput}
+            type="text"
+            autoComplete="off"
+            autoFocus
+            placeholder="Search images and photos"
+          />
+        </form>
+      </header>
+    );
+  }
+}
