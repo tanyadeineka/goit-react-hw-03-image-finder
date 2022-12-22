ed0070da01262cc6687bf0fe8f7d785c73b483dd
Tetiana Deineka <tata4ka666@i.ua>
2022-12-22 15:24:39 +0200

    Update pixabay-api.jsx

--- src/components/pixabay-api.jsx
+++ src/components/pixabay-api.jsx
@@ -4,16 +4,9 @@ axios.defaults.baseURL = `https://pixabay.com/api/`;
 
 const KEY = `31491056-913106eba1c8b28fe3dc938e7`;
 
-export const fetchImages = async (inputValue, pageNumber) => {
+export const fetchImages = async (query, page) => {
     const response = await axios.get(
-      `/?q=${inputValue}&page=${pageNumber}&key=${KEY}&image_type=photo&orientation=horizontal&per_page=12`
+      `/?q=${query}&page=${page}&key=${KEY}&image_type=photo&orientation=horizontal&per_page=12`
     );
-    return response.data.hits.map(image => {
-        return {
-          id: image.id,
-          webformatURL: image.webformatURL,
-          largeImageURL: image.largeImageURL,
-          tags: image.tags,
-        };
-    })
+  return response.data;
 }
