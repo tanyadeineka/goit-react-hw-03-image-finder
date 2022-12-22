cb0ac645fb04cad773c5127ea195966d27d76c03
Tetiana Deineka <tata4ka666@i.ua>
2022-12-22 15:24:08 +0200

    Update ImageGalleryItem.jsx

--- src/components/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -2,14 +2,14 @@ import propTypes from 'prop-types';
 import css from './ImageGalleryItem.module.css';
 
 export const ImageGalleryItem = ({ image, onclick }) => (
-    <li className={css.ImageGalleryItem} id={image.id} onClick={onclick}>
-      <img
-        className={css.ImageGalleryItemImage}
-        src={image.webformatURL}
-        alt={image.tags}
-        name={image.largeImageURL}
-      />
-    </li>
+  <li className={css.ImageGalleryItem} id={image.id} onClick={onclick}>
+    <img
+      src={image.webformatURL}
+      alt={image.tags}
+      name={image.largeImageURL}
+      className={css.ImageGalleryItemImage}
+    />
+  </li>
 );
 
 ImageGalleryItem.propTypes = {
