20cd15652a543e31c7d50b9f562ed85f7978b3af
Tetiana Deineka <tata4ka666@i.ua>
2022-12-26 14:40:35 +0200

    Update ImageGalleryItem.jsx

--- src/components/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -1,24 +1,24 @@
 import propTypes from 'prop-types';
 import css from './ImageGalleryItem.module.css';
 
-export const ImageGalleryItem = ({ image, onImageClick, tags, largeImageURL }) => {
+export const ImageGalleryItem = ({ smallImg, largeImg, alt, openModal }) => {
   return (
     <li
       className={css.ImageGalleryItem}
-      id={image.id}
-      onClick={() => onImageClick(largeImageURL, tags)}
+      onClick={() => openModal(largeImg, alt)}
     >
       <img
-        src={image.webformatURL}
-        alt={image.tags}
-        name={image.largeImageURL}
+        src={smallImg}
+        alt={alt}
         className={css.ImageGalleryItemImage}
       />
     </li>
   );
-}
+};
 
 ImageGalleryItem.propTypes = {
-  image: propTypes.object.isRequired,
-  onImageClick: propTypes.func.isRequired,
+  smallImg: propTypes.string.isRequired,
+  largeImg: propTypes.string.isRequired,
+  alt: propTypes.string.isRequired,
+  openModal: propTypes.func.isRequired,
 };
